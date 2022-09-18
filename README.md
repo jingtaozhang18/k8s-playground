6b887826610f7e7ba4b6b973b7b794ccdce7a74c
jingtao <851207685@qq.com>
2022-09-18 18:21:48 +0800

    update

--- README.md
+++ README.md
@@ -201,12 +201,14 @@ Deploy it using below command.
 ```bash
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm repo update
-CONTEXT_NAME="playground"
+PROFILE_NAME="playground"
+CONTEXT_NAME=PROFILE_NAME
 NFS_STORAGE_NAMESPACE="storage-nfs"
 # get host ip
 BR0_IP=$(ip addr show br0 |grep inet |grep -v 127.0.0.1 |grep -v inet6 |awk '{print $2}' |tr -d "addr:")
 BR0_IP=$(echo ${BR0_IP//\// } |awk '{print $1}')
 echo "your host ip: ${BR0_IP}"
+minikube kubectl --profile ${PROFILE_NAME} -- create namespace ${NFS_STORAGE_NAMESPACE}
 helm upgrade --install nfs-subdir-external-provisioner            \
   --kube-context ${CONTEXT_NAME}                                  \
   --namespace ${NFS_STORAGE_NAMESPACE}                            \
--- scripts/k8s_infra_services_enable.sh
+++ scripts/k8s_infra_services_enable.sh
@@ -17,37 +17,22 @@ if [[ ${HELM_INSTALLED} == "n" ]]; then
   exit 1
 fi
 
-CONTEXT_NAME="playground"
+PROFILE_NAME="playground"
+CONTEXT_NAME=PROFILE_NAME
 NFS_STORAGE_NAMESPACE="storage-nfs"
 INFRA_NAMESPACE="infra"
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
-
 helm repo update
 
 # get host ip
-BR0_IP=$(ip addr show br0 | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:")
-BR0_IP=$(echo ${BR0_IP//\// } | awk '{print $1}')
+BR0_IP=$(ip addr show br0 |grep inet |grep -v 127.0.0.1 |grep -v inet6 |awk '{print $2}' |tr -d "addr:")
+BR0_IP=$(echo ${BR0_IP//\// } |awk '{print $1}')
 echo "your host ip: ${BR0_IP}"
-helm upgrade --install nfs-subdir-external-provisioner \
-  --kube-context ${CONTEXT_NAME} \
-  --namespace ${NFS_STORAGE_NAMESPACE} \
-  --values configs/charts_values/nfs-values.yaml \
-  --set nfs.server=${BR0_IP} \
+minikube kubectl --profile ${PROFILE_NAME} -- create namespace ${NFS_STORAGE_NAMESPACE} --dry-run=client -o yaml | minikube kubectl --profile ${PROFILE_NAME} -- apply -f -
+helm upgrade --install nfs-subdir-external-provisioner            \
+  --kube-context ${CONTEXT_NAME}                                  \
+  --namespace ${NFS_STORAGE_NAMESPACE}                            \
+  --values configs/charts_values/nfs-values.yaml                  \
+  --set nfs.server=${BR0_IP}                                      \
   nfs-subdir-external-provisioner/nfs-subdir-external-provisioner
-
-helm upgrade --install bitnami-kube-prometheus \
-  --namespace ${INFRA_NAMESPACE} \
-  --values ${WORKING_DIR}/configs/charts_values/kube-prometheus-values.yaml \
-  bitnami/kube-prometheus
-
-helm upgrade --install bitnami-mysql \
-  --namespace ${INFRA_NAMESPACE} \
-  --values ${WORKING_DIR}/configs/charts_values/mysql-values.yaml \
-  bitnami/mysql
-
-helm upgrade --install bitnami-grafana-operator \
-  --namespace ${INFRA_NAMESPACE} \
-  --values ${WORKING_DIR}/configs/charts_values/grafana-operator-values.yaml \
-  bitnami/grafana-operator
