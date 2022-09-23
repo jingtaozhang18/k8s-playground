0d6eedd1b51979e9910a7448f8e3602608bc2199
jingtao <851207685@qq.com>
2022-09-23 23:16:31 +0800

    update

--- scripts/k8s_start.sh
+++ scripts/k8s_start.sh
@@ -22,25 +22,27 @@ minikube version
 # using transparent proxy instead http/https proxy
 # start minikube
 PROFILE_NAME="playground"
-SOFT_ROUTE_IP="192.168.1.41"
+SOFT_ROUTE_IP="192.168.1.1"
 NODE_NUM=4
-bash ${WORKING_DIR}/scripts/k8s_set_route.sh ${PROFILE_NAME} ${NODE_NUM} ${SOFT_ROUTE_IP} &
+# bash ${WORKING_DIR}/scripts/k8s_set_route.sh ${PROFILE_NAME} ${NODE_NUM} ${SOFT_ROUTE_IP} &
+# export HTTP_PROXY=http://${SOFT_ROUTE_IP}:1080
+# export HTTPS_PROXY=https://${SOFT_ROUTE_IP}:1080
+# export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24
 minikube \
   --profile ${PROFILE_NAME} \
   --driver=kvm2 \
   --install-addons=false \
-  --kubernetes-version v1.24.3 \
+  --kubernetes-version='v1.24.3' \
   --auto-update-drivers=false \
-  --nodes ${NODE_NUM} \
-  --cpus 6 \
-  --memory 12g \
-  --disk-size 40g \
+  --nodes=${NODE_NUM} \
+  --cpus=6 \
+  --memory=12g \
+  --disk-size=40g \
   --kvm-network='bridged-network' \
+  --image-mirror-country='cn' \
+  --image-repository='registry.cn-hangzhou.aliyuncs.com/google_containers' \
   start
-
-minikube --profile ${PROFILE_NAME} addons disable storage-provisioner
-minikube --profile ${PROFILE_NAME} addons disable default-storageclass
 
 minikube kubectl --profile ${PROFILE_NAME} -- get pods -A
 
-minikube dashboard --profile ${PROFILE_NAME} --url
+minikube dashboard --profile ${PROFILE_NAME} --url &
