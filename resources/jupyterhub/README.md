e480f1e2e13d007dc3f7957d4cc09d91c2e90ee1
jingtao <851207685@qq.com>
2022-10-18 21:40:42 +0800

    update

---
+++ resources/jupyterhub/README.md
@@ -0,0 +1,5 @@
+# Jupyter Hub
+
+```bash
+bash env_init.sh
+```
--- resources/jupyterhub/init_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/opt/bitnami/miniconda/bin/conda install -y openjdk
-/opt/bitnami/miniconda/bin/conda install -y pyspark
